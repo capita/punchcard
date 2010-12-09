0dc88e1ff0ccd9274d642cc0cf1a564a15cd8b8a
Christoph Olszowka <christoph@olszowka.de>
2010-12-09 08:42:01 +0100

    Active Record related fixes

--- lib/punchcard.rb
+++ lib/punchcard.rb
@@ -9,7 +9,7 @@ require 'logger'
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.logger.level = Logger::WARN
 ActiveRecord::Migration.verbose = false
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || {:adapter => 'sqlite3', :database => "punchcard_#{ENV['RACK_ENV'] || 'dev'}.sqlite3"})
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || {:adapter => 'sqlite3', :database => "punchcard_#{ENV['RACK_ENV'] || 'development'}.sqlite3"})
 
 require 'punchcard/person'
 require 'punchcard/punch'
@@ -23,7 +23,7 @@ class Punchcard < Sinatra::Base
   end
   
   get '/status.json' do
-    Person.order_by(:name).all.to_json
+    Person.order('name ASC').map(&:payload).to_json
   end
   
   post '/punch/:id' do
